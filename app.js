9011aa343d3d5fef5d9f27d00937e1828d27d900
Mike DeAngelis <mikedeangelis@MikeMacbookPro.local>
2024-04-24 15:07:34 -0400

    Add console logging to sentry

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ Sentry.init({
     new Sentry.Integrations.Http({ tracing: true }),
     // enable Express.js middleware tracing
     new Tracing.Integrations.Express({ app }),
+    new CaptureConsole({
+      levels: ['log', 'info', 'warn', 'error', 'debug', 'assert'],
+    }),
   ],
 
   // Set tracesSampleRate to 1.0 to capture 100%
