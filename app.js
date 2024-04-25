dff9e9054f0e7b9bed5bf9ab4c62b00aad669d77
Mike DeAngelis <mikedeangelis@MikeMacbookPro.local>
2024-04-24 20:02:55 -0400

    Add environment to sentry

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ Sentry.init({
       levels: ['log', 'info', 'warn', 'error', 'debug', 'assert'],
     }),
   ],
-
+  environment: process.env.NODE_ENV,
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 /*
   Okay folks, want to learn a little bit about webpack?
 */
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const env = require("dotenv").config({ path: "variables.env" });
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const env = require('dotenv').config({ path: 'variables.env' });
 const nodeEnv = process.env.NODE_ENV || env.parsed.NODE_ENV;
 
 /*
@@ -17,9 +17,9 @@ const javascript = {
   test: /\.(js)$/, // see how we match anything that ends in `.js`? Cool
   exclude: /(node_modules)/,
   use: {
-    loader: "babel-loader",
+    loader: 'babel-loader',
     options: {
-      presets: ["@babel/preset-env"],
+      presets: ['@babel/preset-env'],
     },
   },
 };
@@ -29,7 +29,7 @@ const javascript = {
 */
 
 const postcss = {
-  loader: "postcss-loader",
+  loader: 'postcss-loader',
   options: {
     postcssOptions: {
       plugins: { autoprefixer: true },
@@ -46,17 +46,17 @@ const styles = {
   use: [
     MiniCssExtractPlugin.loader,
     {
-      loader: "css-loader",
+      loader: 'css-loader',
       options: {
         sourceMap: true,
       },
     },
     {
-      loader: "resolve-url-loader",
+      loader: 'resolve-url-loader',
     },
     postcss,
     {
-      loader: "sass-loader",
+      loader: 'sass-loader',
       options: {
         sourceMap: true,
       },
@@ -67,19 +67,19 @@ const styles = {
 // OK - now it's time to put it all together
 const config = {
   mode: nodeEnv,
-  devtool: "source-map",
+  devtool: 'source-map',
   entry: {
     // we only have 1 entry, but I've set it up for multiple in the future
-    App: "./public/javascripts/delicious-app.js",
+    App: './public/javascripts/delicious-app.js',
   },
   // Once things are done, we kick it out to a file.
   output: {
     // path is a built in node module
     // __dirname is a variable from node that gives us the
-    path: path.resolve(__dirname, "public", "dist"),
+    path: path.resolve(__dirname, 'public', 'dist'),
     // we can use "substitutions" in file names like [name] and [hash]
     // name will be `App` because that is what we used above in our entry
-    filename: "[name].bundle.js",
+    filename: '[name].bundle.js',
   },
 
   // remember we said webpack sees everthing as modules and how different loaders are responsible for different file types? Here is is where we implement them. Pass it the rules for our JS and our styles
@@ -89,7 +89,7 @@ const config = {
   // finally we pass it an array of our plugins
   plugins: [
     // here is where we tell it to output our css to a separate file
-    new MiniCssExtractPlugin({ filename: "style.css" }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
   ],
 };
 
